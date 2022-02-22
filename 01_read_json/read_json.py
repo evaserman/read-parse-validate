46fce6de7846c559f11358ad15d69760ceb8224e
evaserman <ellieav@mit.edu>
2022-02-22 10:32:39 -0500

    commit

--- 01_read_json/read_json.py
+++ 01_read_json/read_json.py
@@ -1,24 +1,29 @@
 # ---------------------------------------------
 # import package to read json
 # ---------------------------------------------
-
+import json
 
 # ---------------------------------------------
 #  write function to load file and parse json
 # ---------------------------------------------
 
 def readJson(file):
-    pass
+    with open(file) as p:
+        return json.load(p)
 
 # ---------------------------------------------
 #  call 'readJson', load salaries
 # ---------------------------------------------
 
+salaries = readJson('./data.json')
+print(salaries)
 
 # ---------------------------------------------
 #  print all salaries
 # ---------------------------------------------
 
+for salary in salaries['data']:
+    print(float(salary[18]))
 
 # ---------------------------------------------
 # loop through list, only print salary field

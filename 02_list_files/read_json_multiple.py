126c6c9e056010b55b4b64aba67651327062e344
evaserman <ellieav@mit.edu>
2022-02-22 10:32:31 -0500

    commit

--- 02_list_files/read_json_multiple.py
+++ 02_list_files/read_json_multiple.py
@@ -2,13 +2,27 @@
 # import package to read json
 # import package to walk file system
 # -------------------------------------
-
+import json
+import glob 
+# glob finds patterns in data to pick up only certain types of data
 
 # -----------------------------------
 #  list all files
 # -----------------------------------
-
+pattern = './data/*/*.json'
+data = []
+for file in glob.glob(pattern):
+    data.append(file)
+    print(data)
 
 # -----------------------------------
 #  loop through files, parse json
 # -----------------------------------
+
+def readJson(file):
+    with open(file) as p:
+        return json.load(p)
+for file in data:
+    file - readJson(file)
+    print(file)
+    
